cb73d21eac8b5f86cadb509aec7226ea77caa36a
acmota2 <acmota2@gmail.com>
2025-09-28 17:30:57 +0200

    feat(flake): adding nix integration

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Flake for Rust+htmx";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustc
+          cargo
+          bacon
+        ];
+      };
+    };
+}
